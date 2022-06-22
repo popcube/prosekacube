e4f95191b4866c109ffb1247c51fa4760f085d52
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 18:21:16 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import { madSkillz } from "./mad_skillz";
-
-const Container = () => {
-  return <madSkillz />;
-};
+import { useState } from "react";
 
 function App() {
+  const [content, showContent] = useState("mad_skillz");
+
+  const Body = () => {
+    switch (content) {
+      case "mad_skillz":
+        return madSkillz;
+      default:
+        return <div>nothing detected</div>;
+    }
+  };
+
   return (
     <div>
       <h1>コンテンツ一覧</h1>
-      <Container />
+      <Body />
     </div>
   );
 }
