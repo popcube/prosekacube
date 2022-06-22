a1101b97177426a225de83fef4b59f4b06d8ee44
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 17:25:31 +0900

    content added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import latestPic from "./docs/figs/latest/2022_06_20.png";
 
-function App() {
+async function ContentSwitch(Content) {
+  switch (Content) {
+    case "mad_skillz":
+      return <div>mad_skillz selected</div>;
+    default:
+      return <div>none selected</div>;
+  }
+}
+
+async function App() {
   return (
     <div>
-      <h4>コンテンツ一覧</h4>
+      <h1>コンテンツ一覧</h1>
       <img src={latestPic} alt="" />
+      {ContentSwitch("mad_skillz")}
     </div>
   );
 }
