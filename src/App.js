884cbf20f9b6a134e40d33db1bbc586f9330d5e3
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 18:39:45 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import "./App.css";
 import { madSkillz } from "./mad_skillz";
 import { useState } from "react";
 
-const Body = () => {
-  return <div>some line</div>;
-  // switch (content) {
-  //   case "mad_skillz":
-  //     return madSkillz;
-  //   default:
-  //     return <div>nothing detected</div>;
-  // }
+const Body = ({ content }) => {
+  // return <div>some line</div>;
+  switch (content) {
+    case "mad_skillz":
+      return (
+        <div>
+          here comes a line
+          <madSkillz />
+        </div>
+      );
+    default:
+      return <div>nothing detected</div>;
+  }
 };
 
 function App() {
+  const [content, showContent] = useState("mad_skillz");
   return (
     <div>
       <h1>コンテンツ一覧</h1>
-      <Body />
+      <Body content={content} />
     </div>
   );
 }
