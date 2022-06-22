85b56e585146ac32221a6bca0284e3462b506c31
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 17:43:21 +0900

    none detected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
 import latestPic from "./docs/figs/latest/2022_06_20.png";
 
-function ContentSwitch(Content) {
-  return <div>none selected</div>;
-  // switch (Content) {
-  //   case "mad_skillz":
-  //     return <div>mad_skillz selected</div>;
-  //   default:
-  //     return <div>none selected</div>;
-  // }
-}
+const Container = () => {
+  return <div>none detected</div>;
+};
 
-async function App() {
+function App() {
   return (
     <div>
       <h1>コンテンツ一覧</h1>
       <img src={latestPic} alt="" />
-      {ContentSwitch("mad_skillz")}
+      <Container />
     </div>
   );
 }
