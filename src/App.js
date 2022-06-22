8551e9ebe212310f2e34c9c63c662f0b5771ab95
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 16:22:22 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ function App() {
   return (
     <div>
       <h4>コンテンツ一覧</h4>
+      <button onClick={showContent("others")}> 終了</button>
       {() => {
         switch (content) {
           case "mad_skillz":
-            return <img src="./docs/figs/latest/2022_06_20.png" />;
+            return (
+              <img src="./docs/figs/latest/2022_06_20.png" alt="latest mad skill requirement" />
+            );
           default:
             return;
         }
