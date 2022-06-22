35add4584ee6d258343936cc36739135f83fb0ab
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 15:20:04 +0900

    swtich statement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { mad_skillz } from "./mad_skillz/mad_skillz";
+import { useState } from "react";
 
 function App() {
+  const [content, showContent] = useState("mad_skillz");
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h4>コンテンツ一覧</h4>
+      {
+        switch(content){
+          case "madSkillz":
+            <mad_skillz />
+          default:
+            ;
+        }
+      }
     </div>
   );
 }
