b86c49237c3a23383468cc1e97a372acb9a6ce26
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-22 15:25:37 +0900

    swtich statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { mad_skillz } from "./mad_skillz/mad_skillz";
+import { madSkillz } from "./mad_skillz/mad_skillz";
 import { useState } from "react";
 
 function App() {
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
       <h4>コンテンツ一覧</h4>
-      {
-        switch(content){
-          case "madSkillz":
-            <mad_skillz />
+      {() => {
+        switch (content) {
+          case "mad_skillz":
+            return madSkillz();
           default:
-            ;
+            return;
         }
-      }
+      }}
     </div>
   );
 }
--- src/mad_skillz/mad_skillz.js
+++ src/mad_skillz/mad_skillz.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const { JSDOM } = require("jsdom");
 
-export async function mad_skillz() {
+export async function madSkillz() {
   const figDir = "docs/figs";
   const latestFigDir = `${figDir}/latest`;
   const templateHtml = "docs/template.html";
