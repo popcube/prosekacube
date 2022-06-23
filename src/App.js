885e71265627bf6caf770f8bbdd5f2bd75a55dc2
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-06-23 10:02:58 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { MadSkillz } from "./mad_skillz";
 import { useState } from "react";
+import styled from "styled-components";
 
 const Body = ({ content }) => {
   // return <div>some line</div>;
   switch (content) {
     case "mad_skillz":
-      return (
-        <div>
-          <MadSkillz />
-        </div>
-      );
+      return <MadSkillz />;
     default:
       return <div>nothing detected</div>;
   }
 };
+
+const Container = styled.Body`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 64px 0;
+  background-color: #757575;
+`;
 
 function App() {
   const [content, showContent] = useState("mad_skillz");
@@ -22,7 +26,7 @@ function App() {
     <div>
       <h1>Contents List</h1>
       <button onClick={() => showContent("nothing")}>erase</button>
-      <Body content={content} />
+      <Container content={content} />
     </div>
   );
 }
