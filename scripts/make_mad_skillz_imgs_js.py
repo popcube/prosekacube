32dfbab035e48598b4f78b3d5a3e8b2a6c5b1060
popcube <40394657+popcube@users.noreply.github.com>
2022-06-26 13:20:42 +0900

    mad_skillz content first release

--- scripts/make_mad_skillz_imgs_js.py
+++ scripts/make_mad_skillz_imgs_js.py
@@ -1,31 +1,72 @@
 import os
 
-import_line = 'import img_num from "./../docs/figs/png"\n'
-import_latest_line = 'import img_latest from "./../docs/figs/latest/png"\n'
-
-## png name will be yyyy_mm_dd.png
-## parse it to match the title format
+import_line = 'import img_num from "./../docs/figs/png";\n'
+import_latest_line = 'import img_latest from "./../docs/figs/latest/png";\n'
+img_container = '      <ImgContainer src={img_num} title="title_line" alt="alt_line" />\n'
+target_start_line_1 = "// python auto-generation part 1 start\n"
+target_end_line_1 = "// python auto-generation part 1 end\n"
+target_start_line_2 = "// python auto-generation part 2 start\n"
+target_end_line_2 = "// python auto-generation part 2 end\n"
 
 latest_png = ""
 png_list = []
+import_lines = []
+img_containers = []
+latest_list = []
+target_data = ""
 
+# read png list
 for f in os.listdir("./src/docs/figs"):
     if os.path.isfile(f"./src/docs/figs/{f}"):
         png_list.append(f)
 
-f_list = os.listdir("./src/docs/figs/latest")
+latest_list = os.listdir("./src/docs/figs/latest")
 
-if len(f_list) == 1 and os.path.isfile(f"./src/docs/figs/latest/{f_list[0]}"):
-    latest_png = f_list[0]
+if len(latest_list) == 1 and os.path.isfile(f"./src/docs/figs/latest/{latest_list[0]}"):
+    latest_png = latest_list[0]
 else:
     print("Only one png file is allowed in the latest directory")
 
-with open("./src/dynamic/mad_skillz_imgs.js", "w") as target_file:
-    target_file.write(import_latest_line
-        .replace("png", latest_png))
-    for i, png in enumerate(png_list):
-        target_file.write(import_line
-            .replace("img_num", f"img_{i}")
-            .replace("png", png))
+# make target lines
+import_lines.append(import_latest_line.replace("png", latest_png))
+img_containers.extend(
+    ["const DynamicImgContainers = () => {\n",
+     "  return (\n",
+     "    <div>\n"]
+)
+for i in range(len(png_list)):
+    import_lines.append(import_line
+                        .replace("img_num", f"img_{i}")
+                        .replace("png", png_list[i]))
+    # png name will be yyyy_mm_dd.png
+    # parse it to match the title format
+    img_containers.append(img_container
+                          .replace("img_num", f"img_{i}")
+                          .replace("title_line", png_list[i][:10].replace("_", "/") + "時点のレベル")
+                          .replace("alt_line", png_list[i][:10].replace("_", "/") + "時点のグラフ"))
+img_containers.extend(
+    ["    </div>\n",
+     "  )\n",
+     "}\n"]
+)
 
- 
+# read original js data
+with open("./src/dynamic/mad_skillz.js", "r", encoding="utf-8") as f:
+    target_data = f.readlines()
+
+# get target line number
+start_num_1 = target_data.index(target_start_line_1)
+end_num_1 = target_data.index(target_end_line_1)
+start_num_2 = target_data.index(target_start_line_2)
+end_num_2 = target_data.index(target_end_line_2)
+
+print(
+    f"mad_skillz.js replaced line indices: {start_num_1}:{end_num_1}, {start_num_2}:{end_num_2}")
+
+# target line replace
+# !!!! replace from last to start to keep index relevant
+target_data[start_num_2 + 1: end_num_2] = img_containers
+target_data[start_num_1 + 1: end_num_1] = import_lines
+
+with open("./src/dynamic/mad_skillz.js", "w", encoding="utf-8") as f:
+    f.writelines(target_data)
--- src/dynamic/mad_skillz.js
+++ src/dynamic/mad_skillz.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 // python auto-generation part 1 start
-import img_latest from "./../docs/figs/latest/2022_06_24.png"
-import img_0 from "./../docs/figs/2021_12_31.png"
-import img_1 from "./../docs/figs/2022_01_31.png"
-import img_2 from "./../docs/figs/2022_02_24.png"
+import img_latest from "./../docs/figs/latest/2022_06_24.png";
+import img_0 from "./../docs/figs/2021_12_31.png";
+import img_1 from "./../docs/figs/2022_01_31.png";
+import img_2 from "./../docs/figs/2022_02_24.png";
+import img_3 from "./../docs/figs/2022_03_31.png";
+import img_4 from "./../docs/figs/2022_04_28.png";
+import img_5 from "./../docs/figs/2022_05_31.png";
 // python auto-generation part 1 end
-
-const img_names = ["2021/12/31", "2022_01_31", "2022_02_24"];
 
 const LatestImgDiv = styled.div`
   padding: 12px 64px 12px;
@@ -15,8 +16,6 @@ const LatestImgDiv = styled.div`
 `;
 
 const ImgDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
   padding: 12px 64px 12px;
   background-color: #BCECE0;
 `;
@@ -27,7 +26,7 @@ const TitleText = styled.text`
   background-color: #BCECE0;
 `
 
-const LatestImgContainer = ({ src, title }) => {
+const LatestImgContainer = ({ src, title, alt }) => {
   return (
     <LatestImgDiv>
       <div align="left">
@@ -36,13 +35,13 @@ const LatestImgContainer = ({ src, title }) => {
         </TitleText>
       </div>
       <div align="left">
-        <img src={src} alt={title} />
+        <img src={src} alt={alt} />
       </div>
     </LatestImgDiv>
   )
 }
 
-const ImgContainer = ({ src, title }) => {
+const ImgContainer = ({ src, title, alt }) => {
   return (
     <ImgDiv>
       <div align="left">
@@ -51,7 +50,7 @@ const ImgContainer = ({ src, title }) => {
         </TitleText>
       </div>
       <div align="left">
-        <img src={src} alt={title} />
+        <img src={src} alt={alt} />
       </div>
     </ImgDiv>
   )
@@ -61,9 +60,12 @@ const ImgContainer = ({ src, title }) => {
 const DynamicImgContainers = () => {
   return (
     <div>
-      <ImgContainer src={img_0} alt="" />
-      <ImgContainer src={img_1} alt="" />
-      <ImgContainer src={img_2} alt="" />
+      <ImgContainer src={img_0} title="2021/12/31時点のレベル" alt="2021/12/31時点のグラフ" />
+      <ImgContainer src={img_1} title="2022/01/31時点のレベル" alt="2022/01/31時点のグラフ" />
+      <ImgContainer src={img_2} title="2022/02/24時点のレベル" alt="2022/02/24時点のグラフ" />
+      <ImgContainer src={img_3} title="2022/03/31時点のレベル" alt="2022/03/31時点のグラフ" />
+      <ImgContainer src={img_4} title="2022/04/28時点のレベル" alt="2022/04/28時点のグラフ" />
+      <ImgContainer src={img_5} title="2022/05/31時点のレベル" alt="2022/05/31時点のグラフ" />
     </div>
   )
 }
@@ -72,7 +74,7 @@ const DynamicImgContainers = () => {
 export const MadSkillz = () => {
   return (
     <div align="center">
-      <LatestImgContainer src={img_latest} title="最新の皆伝レベル" />
+      <LatestImgContainer src={img_latest} title="最新の皆伝レベル" alt="最新の皆伝レベルグラフ" />
       <DynamicImgContainers />
     </div>
   );
