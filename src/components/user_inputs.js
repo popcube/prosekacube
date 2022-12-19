06725342e26a75b26edbbca6bf683b5184869a2f
popcube <40394657+popcube@users.noreply.github.com>
2022-12-19 13:07:48 +0900

    debug

--- src/components/user_inputs.js
+++ src/components/user_inputs.js
@@ -313,6 +313,7 @@ export const UserInput = () => {
               <CalcSpan>{challengeLiveDueNum(latestRecordBuffer)}</CalcSpan>
               回で達成します
             </TextDiv>
+            <TextDiv>※　デイリーボーナスポイントは含みません</TextDiv>
           </div>
         </InputDiv2>
       )}
