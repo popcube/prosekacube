322bc990755628e89665c8e5f4fe159fe61c5cb9
popcube <40394657+popcube@users.noreply.github.com>
2023-01-08 17:31:47 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function App() {
           </HeaderFaqDiv>
         </HeaderDiv>
       </Header>
-      <hr style={{ color: "#D0D0D0", margin: 0 }} />
+      <hr style={{ backgroundColor: "#D0D0D0", margin: 0, width: "100%", borderWidth: 0, height: "2px" }} />
       <Container>
         <Body content={content} />
       </Container>
