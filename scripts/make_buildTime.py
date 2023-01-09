f11153a9c02c35ea19fed10c6f630eb122456cdf
popcube <40394657+popcube@users.noreply.github.com>
2023-01-09 11:44:41 +0900

    build time JST

--- scripts/make_buildTime.py
+++ scripts/make_buildTime.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
-now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+now = datetime.now(timezone(timedelta(hours=9))).strftime("%Y/%m/%d %H:%M:%S")
 print("build time: " + now)
 with open("./src/dynamic/buildTime.js", "w") as f:
     f.write('const buildTime = "' + now + '";\n')
