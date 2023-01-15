fd013492469e1fa72e8be61d0aa62ed95953bc19
popcube <40394657+popcube@users.noreply.github.com>
2023-01-15 17:56:28 +0900

    debug

--- src/twt_api.js
+++ src/twt_api.js
@@ -11,62 +11,94 @@ import {
   ActionText,
   CalcSpan,
 } from "./components/styled_tags";
+import styled from "styled-components";
 
 
-const xArr = [...Array(1000)].map((_, i) => i)
-const yArr = [...Array(1000)].map((_, i) => i * i)
-console.log(xArr)
-console.log(yArr)
+// const xArr = [...Array(1000)].map((_, i) => i)
+// const yArr = [...Array(1000)].map((_, i) => i * i)
+// console.log(xArr)
+// console.log(yArr)
+
+const PlotlyDiv = styled(InputDiv)`
+  margin: 30px 0px;
+`
 
 export default function main() {
   const [show1, setShow1] = useState(false);
-  const ifShowed1 = useRef(null);
-
-  useEffect(() => {
-    if (show1 && ifShowed1.current != null) {
-      ifShowed1.current.style.opacity = "1";
-      ifShowed1.current.style.transform = "none";
-    }
-    else if (!show1 && ifShowed1.current != null) {
-      true;
-      ifShowed1.current.style.opacity = "0";
-      ifShowed1.current.style.transform = "translateY(-100%)";
-    }
-  }, [ifShowed1.current]);
 
   return (
     <div>
+      <PlotlyDiv>
+        <Plot
+          data={[
+            {
+              x: twt_data.raw_1H.x,
+              y: twt_data.raw_1H.y,
+              type: 'scatter',
+              mode: 'lines',
+              marker: { color: 'black' },
+            }
+          ]}
+          layout={{ width: 1000, height: 800, title: 'フォロワー数推移' }}
+        />
+      </PlotlyDiv>
+      <PlotlyDiv>
+        <Plot
+          data={[
+            {
+              x: twt_data.cut_diff_1H.x,
+              y: twt_data.cut_diff_1H.y,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'grey' },
+              name: "フィルター後データ"
+            },
+            {
+              x: twt_data.trend_1H.x,
+              y: twt_data.trend_1H.y,
+              type: 'scatter',
+              mode: 'lines',
+              marker: { color: 'blue' },
+              name: "トレンドライン"
+            },
+          ]}
+          layout={{
+            width: 1000,
+            height: 800,
+            title: 'フォロワー数増減量（/分）推移',
+            annotations: [            // all "annotation" attributes: #layout-annotations
+              {
+                text: 'simple annotation',    // #layout-annotations-text
+                x: 0,                         // #layout-annotations-x
+                xref: 'paper',                // #layout-annotations-xref
+                y: 0,                         // #layout-annotations-y
+                yref: 'paper'                 // #layout-annotations-yref
+              },],
+            showlegend: true,
+            legend:
+            {
+              x: 1,
+              xanchor: 'right',
+              y: 1
+            }
+          }}
+        />
+      </PlotlyDiv>
+
       <div onClick={() => setShow1(!show1)}>click here</div>
       {show1 && (
         <InputDiv>
           <Plot
             data={[
               {
-                x: twt_data[0].x,
-                y: twt_data[0].y,
+                x: twt_data.raw_1H.x,
+                y: twt_data.raw_1H.y,
                 type: 'scatter',
                 mode: 'lines+markers',
-                marker: { color: 'red' },
+                marker: { color: 'black' },
               }
             ]}
-            layout={{ width: 1000, height: 800, title: 'A Fancy Plot' }}
-          />
-        </InputDiv>
-      )
-      }
-      {show1 && (
-        <InputDiv>
-          <Plot
-            data={[
-              {
-                x: twt_data[0].x,
-                y: twt_data[0].y,
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: { color: 'red' },
-              }
-            ]}
-            layout={{ width: 1000, height: 800, title: 'A Fancy Plot' }}
+            layout={{ width: 1000, height: 800, title: 'フォロワー数推移' }}
           />
         </InputDiv>
       )
