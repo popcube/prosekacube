e240ade0402998d8c24bb8614c40aee22ef4fd2d
popcube <40394657+popcube@users.noreply.github.com>
2023-03-24 00:51:47 +0900

    use requests module for https

--- scripts/fetch_song_list.py
+++ scripts/fetch_song_list.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import ssl
+import requests
 
-ssl._create_default_https_context = ssl._create_unverified_context
+pjsekai_res = requests.get("https://pjsekai.com/?aad6ee23b0")
 
-a = pd.read_html("https://pjsekai.com/?aad6ee23b0", encoding="utf-8", attrs={"id": "sortable_table1"})[0]
+a = pd.read_html(pjsekai_res.content, encoding="utf-8",
+                 attrs={"id": "sortable_table1"})[0]
 a.to_csv("./docs/fetched_song_data.csv", index=False, header=False)
